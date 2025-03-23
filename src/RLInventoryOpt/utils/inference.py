e2eaac2ed9768f6d12d477f519619a448b976bb6
Sebastian Sarasti <sebitas.alejo@hotmail.com>
2025-03-22 21:14:21 -0500

    FIX: Solving inference function

--- src/RLInventoryOpt/utils/inference.py
+++ src/RLInventoryOpt/utils/inference.py
@@ -9,14 +9,16 @@ def inference_values(q_table, choices, forecast, initial_state, min_order, lead_
         q_table (np.array): Q-Table with the Q-values of each state-action pair
         choices (list): List of possible actions
         forecast (np.array): Numpy array of forecasted demand
-        initial_state (dict): Dictionary with the initial state of the inventory
+        initial_state (int): Initial stock value of the inventory
         min_order (int): Minimum order value
+        lead_time (int): Lead time (in time units) for the orders
 
     Returns:
-        inventory_levels (list): List of inventory levels for each month
-        optimal_actions (list): List of optimal actions for each month
+        inventory_levels (list): List of inventory levels for each period
+        optimal_actions (list): List of optimal actions for each period
         forecast (np.array): Numpy array of forecasted demand
-        ordered_amount (list): List of ordered amount for each month
+        orders_placed (list): List of ordered amounts for each period
+        orders_received (list): List of orders received for each period
     """
     inventory_levels = []
     optimal_actions = []
@@ -39,20 +41,14 @@ def inference_values(q_table, choices, forecast, initial_state, min_order, lead_
         # Determine the amount to order based on the best action
         if best_action == "no":
             order_value = 0
-        elif best_action == "m":
-            order_value = min_order
-        elif best_action == "2m":
-            order_value = 2 * min_order
-        elif best_action == "3m":
-            order_value = 3 * min_order
-        elif best_action == "4m":
-            order_value = 4 * min_order
-        elif best_action == "5m":
-            order_value = 5 * min_order
-        elif best_action == "6m":
-            order_value = 6 * min_order
+        else:
+            try:
+                multiplier = int(best_action.rstrip("m"))
+            except ValueError:
+                multiplier = 0  # En caso de error, se asigna 0
+            order_value = multiplier * min_order
 
-        # record the order placed
+        # Record the order placed in the current period
         orders_placed[iteration] = order_value
 
         if order_value > 0:
